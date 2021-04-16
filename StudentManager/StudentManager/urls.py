90d10fcd939c558a645ca6756770b0c905986b99
wkl949429516 <wklwukailun.cool@163.com>
2021-04-16 23:10:24 +0800

    一对多修改

--- StudentManager/StudentManager/urls.py
+++ StudentManager/StudentManager/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
 
     re_path(r'^students/', students.findstudent),
     re_path(r'^add_student/', students.add_student),
+    re_path(r'^del_student/', students.del_student),
+    re_path(r'^edit_student/', students.edit_student),
 ]
--- StudentManager/app01/students.py
+++ StudentManager/app01/students.py
@@ -19,7 +19,32 @@ def add_student(request):
     else:
         name = request.POST.get("name")
         class_id = request.POST.get("class_id")
-        sql = "insert into student (name, class_id) values ('{}','{}')".format(name, class_id);
+        sql = "insert into student (name, class_id) values ('{}','{}');".format(name, class_id)
         db.commit(sql)
         return redirect('/students/')
 
+
+def del_student(request):
+    nid = request.GET.get('nid')
+    sql = "delete from student where id = {};".format(nid)
+    db.commit(sql)
+    return redirect('/students/')
+
+
+def edit_student(request):
+    if request.method == "GET":
+        nid = request.GET.get('nid')
+        sql = "select student.id, student.name, class.title, class_id from student left join class on student.class_id = class.id WHERE student.id = {};".format(nid)
+        student_list = db.findone(sql)
+        SqlClass = "select id, title from class;"
+        class_list = db.findmany(SqlClass)
+        data = {"student_list": student_list, "class_list": class_list}
+        return render(request, 'edit_student.html', data)
+    else:
+        nid = request.GET.get('nid')
+        name = request.POST.get('name')
+        class_id = request.POST.get("class_id")
+        sql = "update student set name = '{}', class_id = {} where id = {}".format(name, class_id, nid)
+        print(sql)
+        db.commit(sql)
+        return redirect('/students/')
